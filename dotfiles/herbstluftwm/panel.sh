abb9f1be1eb054004752d651f6af14f0edbffe0b
Carl Sverre <carl@memsql.com>
2020-12-22 12:06:30 -0800

    wallpapers and  tweaks

--- dotfiles/herbstluftwm/panel.sh
+++ dotfiles/herbstluftwm/panel.sh
@@ -6,5 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar -r bottom &
+# Launch Polybar on each moniitor, using default config location
+# ~/.config/polybar/config
+TRAY_POS=right
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload bottom &
+    unset TRAY_POS
+done
