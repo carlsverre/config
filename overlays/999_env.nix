341e74b5f8e57f16fb1ab5a79f7acf0c94506c83
Carl Sverre <id@c83.me>
2022-04-07 14:50:02 -0700

    tweaks

--- overlays/999_env.nix
+++ overlays/999_env.nix
@@ -1,12 +1,9 @@
 self: super:
 
 let
-  # https://github.com/NixOS/nixpkgs/commits/release-20.09
-  # May 24 2021
-  pkgs-stable = import (fetchTarball "https://github.com/nixos/nixpkgs/archive/b8f96258307d2f38df2c3674ea89eb10b7d6c4c5.tar.gz") {};
-
-  # nixpkgs-unstable Nov 24 2020
-  pkgs-unstable = import (fetchTarball "https://github.com/nixos/nixpkgs/archive/6625284c397b44bc9518a5a1567c1b5aae455c08.tar.gz") {};
+  # https://github.com/NixOS/nixpkgs/commits/release-21.11
+  # Feb 28 2022
+  pkgs-stable = import (fetchTarball "https://github.com/nixos/nixpkgs/archive/7bb8599a66395bec86a96aa2d2db44cce2aa3955.tar.gz") {};
 in
   {
     dev-env = super.buildEnv {
@@ -33,7 +30,7 @@ in
         pkgs-stable.autojump
         pkgs-stable.direnv
         pkgs-stable.fasd
-        pkgs-unstable.fzf
+        pkgs-stable.fzf
         pkgs-stable.jq
         pkgs-stable.patchelf
         pkgs-stable.ripgrep
